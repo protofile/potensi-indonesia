21fcd674670ad36c54c3f984124d55b41a7ca215
yudantoardi <yudantoardi@gmail.com>
2024-01-22 15:45:32 +0700

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
-    
+    $(".door").click(function(){
+        $(".prolog").addClass("scene-closed");
+        $(".cockpit-bg").addClass("show");
+        setTimeout(function(){
+            $(".prolog").hide();
+        }, 4000);
+    });
 
 }); 
