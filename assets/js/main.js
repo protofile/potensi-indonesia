8f092e9c1334f53755bab39a5daec7ea33fb512d
yudantoardi <yudantoardi@gmail.com>
2024-01-22 18:01:24 +0700

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
 
-    $(".door").click(function(){
+    $(".prolog .text a").click(function(pa){
+        pa.preventDefault();
+        $(".prolog .text").hide();
         $(".prolog").addClass("scene-closed");
+        $(".scene.cokcpit").addClass("active");
         $(".cockpit-bg").addClass("show");
         setTimeout(function(){
             $(".prolog").hide();
         }, 4000);
+        setTimeout(function(){
+            $("#dialog-1").addClass("show");
+        }, 4000);
+    });
+
+    $(".dialog-box .next").click(function(nx){
+        nx.preventDefault();
+        $(this).parent().parent().removeClass("show");
+        $($(this).attr("href")).addClass("show");
+        
     });
 
 }); 
