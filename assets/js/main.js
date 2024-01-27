32a0c994f15fc6a6eb0295e5194f0ccd409a9d7b
yudantoardi <yudantoardi@gmail.com>
2024-01-27 20:30:41 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
 
+    /*CURSOR*/
+   $(document).mousemove(function(e) {
+        $('.space-cursor').eq(0).css({left: e.pageX,top: e.pageY});
+        setTimeout(function() {
+            $('.space-cursor-2').eq(0).css({left: e.pageX,top: e.pageY});
+        }, 100);
+    })
+    $("a").mousemove(function(e) {
+        $('.space-cursor').eq(0).css({'transform': 'scale(1.5)'});
+    })
+    $("a").mouseleave(function(e) {
+        $('.space-cursor').eq(0).css({'transform': 'scale(1)'});
+    })
+
     $(".prolog .text a").click(function(pa){
         pa.preventDefault();
         $(".prolog .text").hide();
@@ -24,7 +38,35 @@ $(document).ready(function(){
         nx.preventDefault();
         $(this).parent().parent().removeClass("show");
         $($(this).attr("href")).addClass("show");
-        
     });
 
+    $(".id-link").click(function(il){
+        il.preventDefault();
+        $(this).parent().parent().removeClass("show");
+        $($(this).attr("href")).addClass("show");
+    });
+
+    $(".go-to-category").click(function(cng){
+        cng.preventDefault();
+        $(".detail-preloader").toggleClass("closed open");
+        setTimeout(function(){
+            window.location.href = "../kategori.html";
+        });
+    });
+
+    $(".open-frame").click(function(of){
+        of.preventDefault();
+        $(".potensi-box").removeClass("show");
+        $(".detail-box iframe").remove();
+        $(".detail-box").append("<iframe src=" + $(this).attr("href") + ">")
+        setTimeout(function() {
+            $(".detail-box").addClass("show");
+        }, 800);
+    });
+    $(".detail-box").click(function(){
+        $(".detail-box").removeClass("show");
+        setTimeout(function() {
+            $(".potensi-box").addClass("show");
+        }, 800);
+    });
 }); 
