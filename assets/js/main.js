9bf167370d4b21724b93c43f1adb6b9c7255b7d3
yudantoardi <yudantoardi@gmail.com>
2024-02-07 14:59:45 +0700

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     /*CURSOR*/
-   $(document).mousemove(function(e) {
+    /*$(document).mousemove(function(e) {
         $('.space-cursor').eq(0).css({left: e.pageX,top: e.pageY});
         setTimeout(function() {
             $('.space-cursor-2').eq(0).css({left: e.pageX,top: e.pageY});
@@ -13,6 +13,18 @@ $(document).ready(function(){
     $("a").mouseleave(function(e) {
         $('.space-cursor').eq(0).css({'transform': 'scale(1)'});
     })
+    $(".close-bt").mousemove(function(e) {
+        $('.space-cursor').eq(0).css({'opacity': '1'});
+    })
+    $(".close-bt").mouseleave(function(e) {
+        $('.space-cursor').eq(0).css({'opacity': '0'});
+    })
+    $(".close-bt").mousemove(function(e) {
+        $('.space-cursor-2').eq(0).css({'opacity': '1'});
+    })
+    $(".close-bt").mouseleave(function(e) {
+        $('.space-cursor-2').eq(0).css({'opacity': '0'});
+    })*/
 
     $(".prolog .text a").click(function(pa){
         pa.preventDefault();
@@ -46,25 +58,21 @@ $(document).ready(function(){
         $($(this).attr("href")).addClass("show");
     });
 
-    $(".go-to-category").click(function(cng){
-        cng.preventDefault();
-        $(".detail-preloader").toggleClass("closed open");
-        setTimeout(function(){
-            window.location.href = "../kategori.html";
-        });
-    });
-
     $(".open-frame").click(function(of){
         of.preventDefault();
         $(".potensi-box").removeClass("show");
         $(".detail-box iframe").remove();
-        $(".detail-box").append("<iframe src=" + $(this).attr("href") + ">")
+        $(".detail-box").append("<iframe src=" + $(this).attr("href") + ">");
+        $('.space-cursor').eq(0).css({'opacity': '0'});
+        $('.space-cursor-2').eq(0).css({'opacity': '0'});
         setTimeout(function() {
             $(".detail-box").addClass("show");
         }, 800);
     });
     $(".detail-box").click(function(){
         $(".detail-box").removeClass("show");
+        /*$('.space-cursor').eq(0).css({'opacity': '1'});
+        $('.space-cursor-2').eq(0).css({'opacity': '1'});*/
         setTimeout(function() {
             $(".potensi-box").addClass("show");
         }, 800);
